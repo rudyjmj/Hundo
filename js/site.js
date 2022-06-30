0405c8515175a1667f63c2866408eaf7a0e8112e
Rudy M <rudyjmoralesjr@gmail.com>
2022-06-29 20:29:31 -0500

    Initial JS Commit
    First commit of the Cento app, but throwing the following error:
    Uncaught ReferenceError ReferenceError: numbers is not defined
        at generateNumbers (<>\Hundo\js\site.js:34:9)
        at getValues (<>\Hundo\js\site.js:15:23)

--- js/site.js
+++ js/site.js
@@ -1,4 +1,61 @@
-//Call Hello World
-function HelloWorld(){
-    alert("Hello World");
+//get the values from the Page
+//starts or controler function
+function getValues(){
+    //get values from the page
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+
+    //We need to validate our input
+    //parse into Integers
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+    if (Number.isInteger(startValue) && Number.isInteger(endValue)){
+        //we call generateNumbers
+        let numbers = generateNumbers(startValue, endValue);
+        //we call displayNumbers
+        displayNumbers(numbers);
+
+    }else{
+        alert("You must enter integers!");
+    }
+}
+
+//generate numbers from the startValue to the endValue
+//logic function(s)
+function generateNumbers(sValue, eValue){
+
+    let number = [];
+
+    //we want to get all numbers from start to end
+
+    for(let index = sValue; index <= eValue; index++){
+        //this will execute in a loop until index = endValue
+        numbers.push(index);
+    }
+    return numbers;
+
+}
+
+//display the numbers and mark the even numbers in bold
+//display or view functions
+function displayNumbers(numbers){
+
+    let templateRows = "";
+
+    for (let index = 0; index < numbers.length; index++) {
+
+        let className = "even";
+        let number = numbers[index];
+
+        if(number % 2 == 0){
+            className = "even";
+        }else{
+            className = "odd";
+        }
+        templateRows += `<tr><td class="${className}" >${number}</td></tr>`;
+    }
+
+    document.getElementById("results").innerHTML = templateRows;
+
 }
